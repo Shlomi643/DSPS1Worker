5aaf9bcdf01e4309c505cac829db517e6bdbabee
Shlomi <avisros@post.bgu.ac.il>
2019-12-04 23:52:37 +0200

    Initial stuff

---
+++ src/main/java/Sentiment.java
@@ -0,0 +1,106 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.json.Json;
+
+public class Sentiment {
+
+    public Sentiment() {
+
+    }
+
+    public int findSentiment(String review) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP sentimentPipeline = new StanfordCoreNLP(props);
+        int mainSentiment = 0;
+        if (review != null && review.length() > 0) {
+            int longest = 0;
+            Annotation annotation = sentimentPipeline.process(review);
+            for (CoreMap sentence : annotation
+                    .get(CoreAnnotations.SentencesAnnotation.class)) {
+                Tree tree = sentence
+                        .get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+
+            }
+        }
+        return mainSentiment;
+    }
+
+    public void printEntities(String review) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize , ssplit, pos, lemma, ner");
+        StanfordCoreNLP NERPipeline =  new StanfordCoreNLP(props);
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(review);
+
+        // run all Annotators on this text
+        NERPipeline.annotate(document);
+
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(TextAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(NamedEntityTagAnnotation.class);
+                System.out.println("\t-" + word + ":" + ne);
+            }
+        }
+
+    }
+
+    public static String getString(String filename) {
+        try {
+            return new String(Files.readAllBytes(Paths.get("src/main/resources/" + filename)));
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = getString("0689835604.json");
+        JSONObject obj = new JSONObject(text);
+        System.out.println(obj);
+        Sentiment sentiment = new Sentiment();
+        JSONArray reviews = obj.getJSONArray("reviews");
+        for (Object o : reviews) {
+            JSONObject review = (JSONObject) o;
+            System.out.println(review.getString("title") + ": " + sentiment.findSentiment(review.getString("text")));
+            sentiment.printEntities(review.getString("text"));
+        }
+
+    }
+}
